4168dde7d82426584ce52af2b70113b2d1fccc7f
supermans1201 <supermans1201@gmail.com>
2017-10-01 19:49:47 +0800

    0

--- src/MuDroid/XmlMutation/XmlVisitor.java
+++
@@ -1,9 +0,0 @@
-package MuDroid.XmlMutation;
-import java.io.IOException;
-import org.dom4j.*;
-public interface XmlVisitor extends Visitor{
-	    public void visit(Node node)throws IOException;
-	    public void visit(String s)throws IOException;
-
-}
-
